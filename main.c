3d656e52f34313e2be61f3ae8916c955dcd1ac25
eskuratovich11 <56356789+eskuratovich11@users.noreply.github.com>
2025-10-29 15:22:15 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,25 +1,57 @@
 #include <stdint.h>
 #include <stm32f10x.h>
 
-void delay(uint32_t ticks) {
-	for (int i=0; i<ticks; i++) {
-		__NOP();
-	}
+void delay(uint32_t cycles) {
+  for (uint32_t i = 0; i < cycles; i++) {
+    __NOP();
+  }
 }
 
-int __attribute((noreturn)) main(void) {
-	// Enable clock for AFIO
-	RCC->APB2ENR |= RCC_APB2ENR_AFIOEN;
-	// Enable clock for GPIOC
-	RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;
-	// Enable PC13 push-pull mode
-	GPIOC->CRH &= ~GPIO_CRH_CNF13; //clear cnf bits
-	GPIOC->CRH |= GPIO_CRH_MODE13_0; //Max speed = 10Mhz
+uint32_t blink_period = 8000000;
+int8_t speed_stage = 0;
 
-    while (1) {
-	    GPIOC->ODR |= (1U<<13U); //U -- unsigned suffix (to avoid syntax warnings in IDE)
-		delay(1000000);
-	    GPIOC->ODR &= ~(1U<<13U);
-	    delay(1000000);
+void check_buttons(void) {
+  if ((GPIOC->IDR & GPIO_IDR_IDR14) == 0) {
+    delay(10000);
+    if ((GPIOC->IDR & GPIO_IDR_IDR14) == 0) {
+      if (speed_stage < 6) {
+        speed_stage++;
+        blink_period /= 2;
+      }
+      while ((GPIOC->IDR & GPIO_IDR_IDR14) == 0) {
+      }
     }
+  }
+
+  if ((GPIOC->IDR & GPIO_IDR_IDR15) == 0) {
+    delay(10000);
+    if ((GPIOC->IDR & GPIO_IDR_IDR15) == 0) {
+      if (speed_stage > -6) {
+        speed_stage--;
+        blink_period *= 2;
+      }
+      while ((GPIOC->IDR & GPIO_IDR_IDR15) == 0) {
+      }
+    }
+  }
+}
+
+int main(void) {
+  RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;
+
+  GPIOC->CRH &= ~(GPIO_CRH_CNF13 | GPIO_CRH_MODE13);
+  GPIOC->CRH |= GPIO_CRH_MODE13_0;
+
+  GPIOC->CRH &=
+      ~(GPIO_CRH_CNF14 | GPIO_CRH_MODE14 | GPIO_CRH_CNF15 | GPIO_CRH_MODE15);
+  GPIOC->CRH |= (GPIO_CRH_CNF14_1 | GPIO_CRH_CNF15_1);
+  GPIOC->ODR |= (GPIO_ODR_ODR14 | GPIO_ODR_ODR15);
+
+  while (1) {
+    check_buttons();
+
+    GPIOC->ODR ^= GPIO_ODR_ODR13;
+
+    delay(blink_period);
+  }
 }
